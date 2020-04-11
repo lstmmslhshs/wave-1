af20c47c1d61432caab579d490c9fcdf17a43e35
lstmmslhshs <62666225+lstmmslhshs@users.noreply.github.com>
2020-04-11 16:20:46 -0400

    Add files via upload

---
+++ aoaf.py
@@ -0,0 +1,13 @@
+length = input("Enter a length in feet: ")
+
+width = input("Enter a width in feet: ")
+
+length = int(length)
+
+width = int(width)
+
+area =(length * width)/43560
+
+area = str(area)
+
+print("the area of the field is: " + area + " acre")
